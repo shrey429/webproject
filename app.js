8cd3f5c343aa718ca0ee66e5882f5c871712f1af
Shrey <shreyparmarr33@gmail.com>
2021-07-11 11:38:02 +0530

    new app

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ var indexRoutes=require("./routes/index");
 
 
 mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
+//mongoose.connect("mongodb://[shrey:rusty@]host1[:port1][,...hostN[:portN]][/[defaultauthdb][?options]]")
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
